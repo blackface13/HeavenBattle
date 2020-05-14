860b8ba14f0226da75dd1a3a4b9d519936edfaed
Black Face <bf.blackface@gmail.com>
2020-05-15 00:47:21 +0700

    Update

--- Assets/Code/2.BUS/Misc/MouseDragBehaviour.cs
+++ Assets/Code/2.BUS/Misc/MouseDragBehaviour.cs
@@ -1,11 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Experimental.UIElements;
 
-public class MouseDragBehaviour : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class MouseDragBehaviour : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    private bool IsDrag = false;//Đang kéo thả
+    private bool IsHoldTap = false;//Đang giữ tap trên màn hình
+    private bool IsInRange = true;
+    private float DelayTimeTap = 1f;
 
     /// <summary>
-    /// This method will be called on the start of the mouse drag
+    /// Nhấn chuột
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        IsHoldTap = true;
+        StartCoroutine(WaitForEnableDrag());
+    }
+
+    /// <summary>
+    /// Chờ sau 1 khoảng thời gian mới có thể kéo được object
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WaitForEnableDrag()
+    {
+        yield return new WaitForSeconds(DelayTimeTap);
+        if (IsHoldTap && IsInRange)
+            IsDrag = true;
+    }
+
+    /// <summary>
+    /// Nhả chuột
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        IsHoldTap = IsDrag = false;
+    }
+
+    /// <summary>
+    /// Bắt đầu kéo
     /// </summary>
     /// <param name="eventData">mouse pointer event data</param>
     public void OnBeginDrag(PointerEventData eventData)
@@ -15,21 +51,34 @@ public class MouseDragBehaviour : MonoBehaviour, IDragHandler, IBeginDragHandler
     }
 
     /// <summary>
-    /// This method will be called during the mouse drag
+    /// Đang kéo
     /// </summary>
     /// <param name="eventData">mouse pointer event data</param>
     public void OnDrag(PointerEventData eventData)
     {
-        transform.localPosition += (Vector3)eventData.delta;
+        if (IsDrag)
+            transform.localPosition += (Vector3)eventData.delta;
     }
 
     /// <summary>
-    /// This method will be called at the end of mouse drag
+    /// Thả
     /// </summary>
     /// <param name="eventData"></param>
     public void OnEndDrag(PointerEventData eventData)
     {
+        IsDrag = false;
         //Debug.Log("End Drag");
         //Implement your funtionlity here
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //if (IsHoldTap)
+            IsInRange = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IsHoldTap = IsInRange = false;
     }
 }
